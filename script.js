75fe533372818f71c80fa1e1247f0312f2cd3cc2
BedretdinovAirat <bedretdinov-a@mail.ru>
2023-05-13 00:27:01 +0300

    Домашка 13.05.2023

---
+++ script.js
@@ -0,0 +1,58 @@
+// 2.2 Переменные. Типы данных
+// 1 mission
+// let a = 10;
+// alert(a);
+// a = 20;
+// alert(a);
+// 2 mission
+// const yearIphone = '09.01.2007';
+// alert(yearIphone);
+// 3 mission
+// const godJs = "Brendan Eich";
+// alert(godJs);
+// 4 mission
+// let num_1 = 10;
+// let num_2 = 2;
+// alert(num_1 + num_2);
+// alert(num_1 - num_2);
+// alert(num_1 * num_2);
+// alert(num_1 / num_2);
+// 5 mission
+// let numberMult = 2**5;
+// let result = numberMult;
+// alert(result);
+// 6 mission
+// let a = 9;
+// let b = 2;
+// result = a % b;
+// alert(result);
+// 7 mission
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num += 1;
+// num -= 1;
+// alert(num);
+// 8 mission
+// let age = prompt('Сколько вам лет?!');
+// alert(age);
+// 9-9.3 mission
+// let user = {
+//     name: 'Airat',
+//     age: 20,
+//     isAdmin: true
+// }
+// user["city of residence"] = "Moscow";
+// user.name = "Vasya";
+// user.age = 30;
+// delete user["city of residence"];
+// 9.4 mission
+// let info = prompt("Какую информацию хотите узнать о пользователе?");
+// alert(user.name);
+// alert(user.age);
+// alert(user.isAdmin);
+// 10 mission
+let user = prompt("Введите ваше имя");
+alert(`Привет, ${user} !`);
